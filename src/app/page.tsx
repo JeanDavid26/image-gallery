0dbc5f04f06a90b33a69da54db3a999e2c065698
JeanDavid26 <duvernayjeandavid@gmail.com>
2024-09-19 18:12:57 +0200

    Added mock data with nav layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
+const mockImageUrls = [
+  'https://utfs.io/f/BJdmT4fcgRnAJXwBujvUTJhcApFeLPq3CMlowKHt5ND6yQIB',
+  'https://utfs.io/f/BJdmT4fcgRnAqOacM6oD46NpC8MySQ0rv1UtVIizGHYmRW5k',
+  'https://utfs.io/f/BJdmT4fcgRnAB0Jvk8fcgRnAZFsPMXeiyTdQm1hSrwWNDJuz'
+]
 
+const mockImages = mockImageUrls.map((url, index)=> {
+  return {
+    id : index + 1,
+    url : url
+  }
+})
 export default function HomePage() {
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        Gallery in progress
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {
+          [...mockImages, ...mockImages, ...mockImages].map((image) => (
+            <div key={image.id} className="w-48">
+              <img src={image.url} />
+            </div>
+          ))
+        }
       </div>
     </main>
   );
